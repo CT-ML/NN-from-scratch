dbcc6baa2b9ae74834badf0887accbc16efec8a8
charbellazzi1 <charbel.azzy@hotmail.com>
2025-03-05 13:58:28 +0200

    in H meeting ma3 khayye l thierry w anthony 3m yjarreb l backpropagation

--- neural-from-scratch/main.py
+++ neural-from-scratch/main.py
@@ -11,8 +11,8 @@ def step_function(x):
     return np.where(x >= 0, 1, 0)
 
 class NeuralNetwork:
-    def __init__(self, nb_of_neurons_per_layer, activation_function_array, learning_rate, momentum_turn):
-        self.nb_of_neurons_per_layer = nb_of_neurons_per_layer
+    def __init__(self, nb_input, nb_of_neurons_per_layer, activation_function_array, learning_rate, momentum_turn):
+        self.nb_of_neurons_per_layer = nb_of_neurons_per_layer.insert(0, nb_input)
         self.activation_function_array = activation_function_array # activation function at the end of each layer
         self.learning_rate = learning_rate
         self.momentum_turn = momentum_turn
@@ -21,16 +21,17 @@ class NeuralNetwork:
         #TODO optimize vector lengths
         self.internal_activity_vector = [] # output before activation function
         self.nonlinear_output_vector = [] # output after activation function
-
+        self.threshold_vector = []
         self.local_gradient_vector = []
 
-        for layer in range(self.nb_of_layers):
+        self.nonlinear_output_vector.append(np.zeros((self.nb_of_neurons_per_layer[0]))) # treat input vector as imaginary layer
+        for layer in range(1, self.nb_of_layers-1, 1):
             self.internal_activity_vector.append(np.zeros((self.nb_of_neurons_per_layer[layer])))
             self.nonlinear_output_vector.append(np.zeros((self.nb_of_neurons_per_layer[layer])))
             self.local_gradient_vector.append(np.zeros((self.nb_of_neurons_per_layer[layer])))
+            self.threshold_vector.append(np.zeros(self.nb_of_neurons_per_layer[layer]))
 
-
-        self.threshold_vector = np.zeros(self.nb_of_layers)
+        
 
         self.activations = {
             "sigmoid": sigmoid,
@@ -47,7 +48,7 @@ class NeuralNetwork:
         # Initialize a lsit of numpy 2d arrays
         self.weights = []
 
-        for layer in range(self.nb_of_layers - 1):
+        for layer in range(self.nb_of_layers - 2):
             # create neurons_in_layer x neurons_in_next_layer matrix for each layer
             self.weights.append(np.random.randn(self.nb_of_neurons_per_layer[layer],self.nb_of_neurons_per_layer[layer + 1]))
 
@@ -60,12 +61,12 @@ class NeuralNetwork:
 
 
     def forward_calculation(self):
-        for layer in range(self.nb_of_layers - 1):
+        for layer in range(1, self.nb_of_layers - 1):
             #TODO add bias
-            self.internal_activity_vector[layer + 1] = np.dot(self.nonlinear_output_vector[layer], self.weights[layer])
+            self.internal_activity_vector[layer] = np.dot(self.nonlinear_output_vector[layer-1], self.weights[layer-1])
             # apply activation function
-            self.nonlinear_output_vector[layer + 1] = self.activations[self.activation_function_array[layer]](self.internal_activity_vector[layer + 1])
-
+            self.nonlinear_output_vector[layer] = self.activations[self.activation_function_array[layer-1]](self.internal_activity_vector[layer])
+            #TODO combine
 def main():
     # Define network parameters
     nb_of_neurons_per_layer = [1, 1, 1]  # Input layer, one hidden layer, output layer
